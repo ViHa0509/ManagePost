963c58347a5215a1646bf2ca9626001d58e2a656
Vi Ha <chuong.nm@sutrixsolutions.com>
2021-08-16 01:02:57 +0700

    storage image post

--- server/posts/models.py
+++ server/posts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.files import File
+from urllib.request import urlopen
+from tempfile import NamedTemporaryFile
 
 # Create your models here.
 class Post(models.Model):
     title = models.TextField(unique=True)
     content = models.TextField(null=True, blank=True)
     author = models.TextField(default='Anonymous', null=True, blank=True)
-    attchment = models.TextField(null=True, blank=True)
+    attchment = models.ImageField(upload_to='images/')
     like = models.IntegerField(default=0, null=True, blank=True)
     created_on = models.DateTimeField(auto_now=True, null=True, blank=True)
     modified_on = models.DateTimeField(auto_now=True, null=True, blank=True)
--- server/server/urls.py
+++ server/server/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from posts.views import PostViewSet, CommentPostViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
@@ -25,6 +27,7 @@ router.register(r'comment', CommentPostViewSet, basename='comment')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
 
